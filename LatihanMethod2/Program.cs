936d9642f1af6a51a43e94f93dedcf41764ad81f
franysia <francobocilff@gmail.com>
2026-01-14 10:36:25 +0700

    Latihan method 2

--- LatihanMethod2/Program.cs
+++ LatihanMethod2/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int jumlahItem;
+
+decimal hargaItem, diskon, totalBayar;
+
+static decimal hitungDiskon(int jml_item)
+{
+    if (jml_item== 5) return 50000m;
+    else if (jml_item == 10) return 75000m;
+    else return 0;
+}
+
+static decimal hitungTotal(int jml_item, decimal hargaItem, decimal diskon)
+{
+    return jml_item * hargaItem + diskon;
+}
+
+
+
+
+Console.WriteLine("Massukan jumlah Item:");
+jumlahItem = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Massukan harga per Item: Rp");
+hargaItem = decimal.Parse(Console.ReadLine());
+
+diskon = hitungDiskon(jumlahItem);
+
+totalBayar = hitungTotal(jumlahItem, hargaItem, diskon);
+
+Console.WriteLine($"Total yang harus dibayar adalah Rp{totalBayar}");
